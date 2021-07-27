dde5c1524bda9d2704a22ee73467b559544d2162
Carlos Chavez <carlos.chavez@aize.io>
2021-07-27 20:38:16 +0200

    closes 3. Create filter left side menu and styles

--- src/components/Menu/Logo.js
+++ src/components/Menu/Logo.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../../assets/logo.svg";
 
 const StyledLogo = styled.div`
   text-align: center;
+  margin-top: 2rem;
 `;
 
 const Logo = () => {
-  return <StyledLogo>Logo</StyledLogo>;
+  return (
+    <StyledLogo>
+      <img src={logo} alt="logo" />
+    </StyledLogo>
+  );
 };
 
 export default Logo;
